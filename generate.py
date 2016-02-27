510f5e1bc47e2a51cda6d6009b23648e768a4c7d
Daniel Vente <danvente@gmail.com>
2016-02-27 13:07:04 +0100

    rewrite generate to use array and DataFrame

--- generate.py
+++ generate.py
@@ -1,20 +1,13 @@
 import numpy as np
-import random as rand
 import sys
+import pandas
 
-data = []
-for m in range(0,int(sys.argv[1])):
-	vec = np.random.normal(0,1,10).tolist()
-	if sum(map(lambda x: x**2, vec)) > 9.34:
-		vec.append(1)
+data = np.array([np.random.normal(0,1,11).tolist() for i in range(0, int(sys.argv[1]))])
+for row in data:
+	if np.sum(map(lambda x: x**2, row[:-2])) > 9.34:
+		row[10] = 1
 	else:
-		vec.append(-1)
+		row[10] = -1 
 
-	data.append(vec)
-
-with open(sys.argv[2],'w') as file:
-	for i in data:
-		for j in i:
-			file.write(str(j)+" ")
-		file.write("\n")
-	
+D = pandas.DataFrame(data)
+D.to_csv("train.dat")
