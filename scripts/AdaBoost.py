efec3008d6c97aabff8e9e5a4075ecdd90b91678
Daniel Vente <danvente@gmail.com>
2016-03-01 12:24:51 +0100

    revert to single-core implementation

--- scripts/AdaBoost.py
+++ scripts/AdaBoost.py
@@ -1,100 +1,88 @@
 import numpy as np
-import numexpr as ne
+import sys
 import pandas
+from sklearn.neighbors import KNeighborsClassifier
 
-def uniformDist(N): 
-	return np.array([1/float(N) for i in range(0,N)])
+def uniformDist(N):
+	w = [1/float(N) for i in range(0,N)]
+	return w
 
-
+def readData(file):
+	vec = []
+	label = []
+	f = open(file,'r')
+	for line in f:
+		line = line.rstrip()
+		array = line.split(" ")
+		vec.append(map(float, array[:-2]))
+		label.append(int(array[-1]))
+	
+	return (vec,label)
 
 class AdaBoost:
-	def __init__(self, data, trails, dist = uniformDist):
-		df = pandas.read_csv(data)
+	def __init__(self, T, data, dataDist, weakLearn):
+		self.T = T
+		#self.data = 
+		self.vec, self.label = readData(data)#dataDist(len(data))
+		self.hyp = weakLearn
+		self.N = len(self.vec)
+		self.err = [0 for i in range(0,self.T)]
+		self.w = [dataDist(len(self.vec)) for t in range(0,self.T+1)]
+		self.p = [[0 for i in range(0,self.N)] for j in range(0,self.T+1)]
+		self.beta = [0 for i in range(0,self.T)]
+		self.train()
 
-		self.T = trails
-		self.N = self.df.shape[0]
-		self.p = np.array([0 for i in range(0,self.N)]) 
-		self.w = [dist(self.N),dist(self.N)]
-		self.b = np.array([0 for i in range(0,self.T)])
-		self.train() 
 
 	def train(self):
 		for t in range(0,self.T):
-			self.w[0] = self.w[1]
-			sumW = np.sum(self.w[0])
-			
-			#vectorized verzion of p^t = w^t/(sum(w^t))
-			self.p = ne.evaluate("w/sumW", local_dict = {'w': self.w[0], 'sumW': sumW })
-			#TODO: split df into vecs and labels
-			err = np.sum(ne.evaluate("p*abs(stump(h)-y)", local_dict = {'p':self.p, 'stump':stump, 'h':self.df.iloc, 'y':self.df.iloc[-1]})) 
+			for i in range(0, self.N):
+				self.p[t][i] = float(self.w[t][i])/float(sum(self.w[t]))
+				self.err[t] += self.p[t][i]*abs(self.hyp(self.vec[i])-self.label[i])
 
-			self.beta[t] = err/(1-err)
+			self.beta[t] = self.err[t]/float(1-self.err[t])
 
-			self.w[1] = ne.evaluate('w*pow(beta,1-(abs(h-y)))', local_dict = {'w':self.w[0]})
+			for i in range(0, self.N):#choose weights
+				self.w[t+1][i] = self.w[t][i]*pow(self.beta[t],1-abs(self.hyp(self.vec[i])-self.label[i]))
 
-def stump(vec,p):
-	if sum(np.multiply(vec,p)) > 9.34:
+	def finalHyp(self, vec):
+		height = 0
+		thresh = 0
+		for t in range(0,self.T):
+			height += (log(1/float(beta[t])))*self.hyp(vec)
+			thresh += (log(1/float(beta[t])))
+		if height > 0.5*thresh:
+			return 1
+		else:
+			return 0
+
+
+# class weakLearn:
+# 	def __init__(self, data):
+# 		self.vec, self.label = readData(data)
+# 		self.kNN = KNeighborsClassifier(n_neighbors = 15, n_jobs = -1)
+# 		#print self.vec
+# 		#print self.label
+# 		self.kNN.fit(self.vec, self.label)
+
+# 	def algo(self, vec):
+# 		print np.random.normal(0,1,1)
+# 		return self.kNN.predict(vec)
+
+def stump(vec):
+	if sum(vec) > 9.34:
 		return 1
 	else:
-		return -1
-# class AdaBoost:
-# 	def __init__(self, T, data, dataDist, weakLearn):
-# 		self.T = T
-# 		self.thresh = 0
-# 		self.vec, self.label = readData(data)#dataDist(len(data))
-# 		self.hyp = weakLearn
-# 		self.N = len(self.vec)
-# 		self.err = [0 for i in range(0,self.T)]
-# 		self.w = [dataDist(len(self.vec)) for t in range(0,self.T+1)]
-# 		self.p = [[0 for i in range(0,self.N)] for j in range(0,self.T+1)]
-# 		self.beta = [0 for i in range(0,self.T)]
-# 		self.t = 0
-# 		self.train()		
+		return 0
 
-# 	def setDist(self, i):
-# 		self.p[self.t][i] = float(self.w[self.t][i])/float(sum(self.w[self.t]))
-
-# 	def setErr(self, i):
-# 		self.err[self.t] += self.p[self.t][i]*abs(self.hyp(self.vec[i])-self.label[i])
-
-# 	def train(self):
-# 		pool = mult.Pool(processes = mult.cpu_count())
-# 		for self.t in range(0,self.T):
-# 			pool.map(self.setDist, range(0,self.N))
-# 			pool.map(self.setErr, range(0,self.N))
-# 			#for i in range(0,self.N):
-# 				#self.p[t][i] = float(self.w[t][i])/float(sum(self.w[t]))
-# 				#self.err[t] += self.p[t][i]*abs(self.hyp(self.vec[i])-self.label[i])
-
-
-# 			self.beta[t] = self.err[t]/float(1-self.err[t])+1
-
-# 			for i in range(0, self.N):
-# 				self.w[t+1][i] = self.w[t][i]*pow(self.beta[t],1-abs(self.hyp(self.vec[i])-self.label[i]))
-				
-# 			self.thresh += (math.log(1/float(self.beta[t])))
-
-# 		self.thresh *= 0.5
-
-# 	def finalHyp(self, vec):
-# 		height = 0
-# 		for t in range(0,self.T):
-# 			height += (math.log(1/float(self.beta[t])))*self.hyp(vec)
-# 		if height > self.thresh:
-# 			return 1
-# 		else:
-# 			return 0
-
-
-# guess = 0
-# a = AdaBoost(100, "train.dat", uniformDist, stump)
-# for t in range(0,100):
-#  	vec = np.random.normal(0,1,10).tolist()
-#   	true = (sum(map(lambda x: x**2, vec)) > 9.34)
-#   	guess += abs(a.finalHyp(vec)-true)
+# true = (sum(map(lambda x: x**2, vec)) > 9.34)
+# guess = a.finalHyp(vec)
 # print guess
-# os.system('play --no-show-progress --null --channels 1 synth %s sine %f' % (0.1, 1000))
-# # print float(guess)/float(1000)
-#D = pandas.read_csv('train.dat')
-#print D
-a = AdaBoost('train.dat', 10)
+# print true
+guess = 0
+a = AdaBoost(100, "train.dat", uniformDist, stump)
+for t in range(0,100):
+ 	vec = np.random.normal(0,1,10).tolist()
+  	true = (sum(map(lambda x: x**2, vec)) > 9.34)
+  	guess += abs(a.finalHyp(vec)-true)
+print guess
+# print float(guess)/float(1000)
